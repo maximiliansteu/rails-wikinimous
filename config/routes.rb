20fa433bef345b8caa5bd6b2e3b756af8d5a8927
Maximilian Steuck <max@fount.io>
2021-11-09 17:03:08 +0100

    c3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'articles/index', to:
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/destroy'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Article.create(title: Faker::Quote.singular_siegler, content: Faker::Quote.famous_last_words, image: Faker::Avatar.image)
+end
